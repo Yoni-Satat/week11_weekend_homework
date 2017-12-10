6ca63757b23ef0d5627ab9ecf4804e0532434aa2
Yoni Satat <yoni.satat@gmail.com>
2017-12-10 20:25:52 +0000

    added rat trap with capacity - tests passed

--- specs/trap_test.js
+++ specs/trap_test.js
@@ -1,23 +1,49 @@
 const Trap = require('../trap.js');
+const Rat = require('../rat.js');
 const assert = require('assert');
 
 describe('Trap', function() {
   let ratTrap;
+  let rat1;
+  let rat2;
+  let rat3;
 
   beforeEach(function() {
-    ratTrap = new Trap("Mouse trap", "2");
+    ratTrap = new Trap("Rat trap", 2);
+    rat1 = new Rat("Rattus enganus");
+    rat2 = new Rat("Rattus Annandalei");
+    rat3 = new Rat("Rattus exulans");
   });
 
   it('should have a type', function() {
-    assert.strictEqual(ratTrap.type, "Mouse trap");
+    assert.strictEqual(ratTrap.type, "Rat trap");
+  });
+
+  it('should have capacity', function() {
+    assert.strictEqual(ratTrap.capacity, 2);
   });
 
   it('should start empty', function() {
-    assert.strictEqual(ratTrap.emptyTrap(), 0);
+    assert.strictEqual(ratTrap.volume.length, 0);
   });
 
-  it('should be able to contain no more than 2 mice', function() {
-    assert.strictEqual(ratTrap.trapForTwoRats(), 2);
+  it('should be able to catch a rat', function() {
+    ratTrap.cauoghtRat(rat1);
+    assert.strictEqual(ratTrap.volume.length, 1);
+  });
+
+  it('should not be able to contain more than 2 rats', function() {
+    ratTrap.emptyTrap();
+    ratTrap.cauoghtRat(rat1);
+    ratTrap.cauoghtRat(rat2);
+    ratTrap.cauoghtRat(rat3);
+    assert.strictEqual(ratTrap.cauoghtRat(), 'trap is full');
+  });
+
+  it('should be able to contain 2 rats', function() {
+    ratTrap.cauoghtRat(rat1);
+    ratTrap.cauoghtRat(rat2);
+    assert.strictEqual(ratTrap.volume.length, 2);
   });
 
 });
--- trap.js
+++ trap.js
@@ -1,17 +1,23 @@
-const Trap = function(type, volume) {
+const Trap = function(type, capacity, volume) {
   this.type = type;
+  this.capacity = capacity;
   this.volume = [];
 }
 
-Trap.prototype.emptyTrap = function() {
+Trap.prototype.emptyTrap = function(rat) {
+  for(let i = 0; i < this.volume.length; i++) {
+    this.volume.unshift(rat);
+  }
   return this.volume.length;
 }
 
-Trap.prototype.trapForTwoRats = function(rat) {
-  for(let i = 0; i < 2; i++) {
+Trap.prototype.cauoghtRat = function(rat) {
+  if(this.volume.length < 2) {
     this.volume.push(rat);
+  } else {
+    return 'trap is full';
   }
   return this.volume.length;
-};
+}
 
 module.exports = Trap;
